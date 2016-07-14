375b61e7c8d604f400fc07e3fe323da8e68f9935
Kévin Gossé <gossekevin1@gmail.com>
2016-07-14 20:08:11 +0200

    Lesson 7: Error handling and ActivityIndicatorIOS

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -4,12 +4,55 @@ import {
   Text,
   TextInput,
   StyleSheet,
-  TouchableHighlight,
-  ActivityIndicatorIOS
+  ActivityIndicatorIOS,
+  TouchableHighlight
 } from 'react-native';
 import api from '../Utils/api';
 import Dashboard from "./Dashboard";
 
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#48BBEC'
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 25,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  searchInput: {
+    height: 50,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+});
 
 export default class Main extends React.Component{
   
@@ -55,6 +98,9 @@ export default class Main extends React.Component{
   };
   
   render() {
+    const showErr = this.state.error ? (<Text> {this.state.error} </Text>) : (<View></View>);
+    const spinner = this.state.isLoading ? (<ActivityIndicatorIOS animating={true} color="#111" size="large" hidesWhenStopped={true}/>) : (<View></View>);
+    
     return(
       <View style={styles.mainContainer}>
         <Text style={styles.title}> Search for a Github User </Text>
@@ -70,52 +116,11 @@ export default class Main extends React.Component{
         >
           <Text style={styles.buttonText}> Search </Text>
         </TouchableHighlight>
+        {spinner}
+        {showErr}
       </View>
     )
   }
 };
 
 
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    padding: 30,
-    marginTop: 65,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: '#48BBEC'
-  },
-  title: {
-    marginBottom: 20,
-    fontSize: 25,
-    textAlign: 'center',
-    color: '#fff'
-  },
-  searchInput: {
-    height: 50,
-    padding: 4,
-    marginRight: 5,
-    fontSize: 23,
-    borderWidth: 1,
-    borderColor: 'white',
-    borderRadius: 8,
-    color: 'white'
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#111',
-    alignSelf: 'center'
-  },
-  button: {
-    height: 45,
-    flexDirection: 'row',
-    backgroundColor: 'white',
-    borderColor: 'white',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    marginTop: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
-  },
-});
