30b000f12470e1461d16f9a5ae4766c1abd70836
Kévin Gossé <gossekevin1@gmail.com>
2016-07-15 05:18:16 +0200

    Lesson 11: Build a Separator UI component

--- App/Components/Profile.js
+++ App/Components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Badge from './Badge';
+import Separator from './Helpers/Separator';
 import {
   Text,
   View,
@@ -51,6 +52,7 @@ export default class Profile extends React.Component{
               <Text style={styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
               <Text style={styles.rowContent}> {userInfo[item]} </Text>
             </View>
+            <Separator />
           </View>
         )
       }
